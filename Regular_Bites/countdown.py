623c508ceb6d40f3996b3e00c453a944190cade0
sundsar <sundsar.ss@gmail.com>
2021-01-13 18:41:24 +0100

    Bite 37. Rewrite a for loop using recursion

---
+++ Regular_Bites/countdown.py
@@ -0,0 +1,16 @@
+def countdown_for(start=10):
+    for i in reversed(range(1, start + 1)):
+        print(i)
+    print('time is up')
+
+
+def countdown_recursive(start=10):
+    print(start)
+    if start == 1:
+        print('time is up')
+        # return
+    else:
+        countdown_recursive(start - 1)
+
+
+countdown_recursive()
