2c211313cc540be3fe308708f0fc8e22cd4f76fc
sundsar <sundsar.ss@gmail.com>
2021-03-01 13:24:01 +0100

    Bite 180. Group names by country

---
+++ Regular_Bites/names_bite180.py
@@ -0,0 +1,26 @@
+from collections import defaultdict
+
+# fake data from https://www.mockaroo.com
+data = """last_name,first_name,country_code
+Watsham,Husain,ID
+Harrold,Alphonso,BR
+Apdell,Margo,CN
+Tomblings,Deerdre,RU
+Wasielewski,Sula,ID
+Jeffry,Rudolph,TD
+Brenston,Luke,SE
+Parrett,Ines,CN
+Braunle,Kermit,PL
+Halbard,Davie,CN"""
+
+
+def group_names_by_country(data: str = data) -> defaultdict:
+    countries = defaultdict(list)
+    for item in data.splitlines()[1:]:
+        ln, fn, cc = item.split(',')
+        name = f"{fn} {ln}"
+        countries[cc].append(name)
+    return countries
+
+
+print(group_names_by_country())
