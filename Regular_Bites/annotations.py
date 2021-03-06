9b22a53fd8257fbb450c28acb35b248b7e194c37
sundsar <sundsar.ss@gmail.com>
2021-03-06 19:16:20 +0100

    Bite 210. Add Type Annotations

---
+++ Regular_Bites/annotations.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+def sum_numbers(numbers: List[int]) -> int:
+    """Sums numbers
+
+    :param numbers: a list of numbers
+    :type numbers: list
+    :raises TypeError: if not all numeric values passed in
+    :return: sum of numbers
+    :rtype: int
+    """
+    pass
+
+
+print(sum_numbers.__doc__)
