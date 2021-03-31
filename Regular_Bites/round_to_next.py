22909de6f5a981e3c45a3b04b3da87597c9ce1da
sundsar <sundsar.ss@gmail.com>
2021-03-31 21:38:13 +0200

    Bite 289. Round to next number -- 1

--- Regular_Bites/round_to_next.py
+++ Regular_Bites/round_to_next.py
@@ -1,17 +1,11 @@
-import math
-
-
 def round_to_next(number: int, multiple: int):
-    if math.remainder(number, multiple) == 0:
+    if number % multiple == 0:
         return number
     else:
         while True:
-            if number < 0 and multiple < 0:
+            if multiple < 0:
                 number = number - 1
             else:
                 number = number + 1
-            if math.remainder(number, multiple) == 0:
+            if number % multiple == 0:
                 return number
-
-
-print(round_to_next(12_345, 42))
