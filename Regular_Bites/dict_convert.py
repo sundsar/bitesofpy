0026d76f745cd16d1ca7d927053d9471cfbcddf2
sundsar <sundsar.ss@gmail.com>
2021-04-29 14:25:58 +0200

    Bite 13. Convert dict to namedtuple/json

---
+++ Regular_Bites/dict_convert.py
@@ -0,0 +1,23 @@
+from collections import namedtuple
+from datetime import datetime
+import json
+
+
+blog = dict(name='PyBites',
+            founders=('Julian', 'Bob'),
+            started=datetime(year=2016, month=12, day=19),
+            tags=['Python', 'Code Challenges', 'Learn by Doing'],
+            location='Spain/Australia',
+            site='https://pybit.es')
+
+# define namedtuple here
+Blog = namedtuple('Blog', blog.keys())
+
+
+def dict2nt(dict_):
+    return Blog._make(dict_.values())
+
+
+def nt2json(nt):
+    d = nt._asdict()
+    return json.dumps(d, default=str)
