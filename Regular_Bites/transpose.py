93513d97ea22fe7a21c5ebe1c056bf913cda53b2
sundsar <sundsar.ss@gmail.com>
2021-05-25 21:53:20 +0200

    Bite 33. Transpose a data structure

---
+++ Regular_Bites/transpose.py
@@ -0,0 +1,23 @@
+def transpose(data):
+    """Transpose a data structure
+    1. dict
+    data = {'2017-8': 19, '2017-9': 13}
+    In:  transpose(data)
+    Out: [('2017-8', '2017-9'), (19, 13)]
+
+    2. list of (named)tuples
+    data = [Member(name='Bob', since_days=60, karma_points=60,
+                   bitecoin_earned=56),
+            Member(name='Julian', since_days=221, karma_points=34,
+                   bitecoin_earned=78)]
+    In: transpose(data)
+    Out: [('Bob', 'Julian'), (60, 221), (60, 34), (56, 78)]
+    """
+    if isinstance(data, dict):
+        return list(zip(*data.items()))
+    else:
+        lst = []
+        for nt in data:
+            d = nt._asdict()
+            lst.append(list(d.values()))
+        return list(zip(*lst))
