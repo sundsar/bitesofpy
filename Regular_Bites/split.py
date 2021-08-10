b04161eda2c7fa8094eed1a0779ff465650efab4
sundsar <sundsar.ss@gmail.com>
2021-08-10 12:33:57 +0200

    Split a string by spaces or quoted text

---
+++ Regular_Bites/split.py
@@ -0,0 +1,16 @@
+from shlex import split
+
+
+def split_words_and_quoted_text(text):
+    """Split string text by space unless it is
+       wrapped inside double quotes, returning a list
+       of the elements.
+
+       For example
+       if text =
+       'Should give "3 elements only"'
+
+       the resulting list would be:
+       ['Should', 'give', '3 elements only']
+    """
+    return split(text)
